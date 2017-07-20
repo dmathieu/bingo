0f689ff8cc6e0560ebc7a094c5efb4c0ba2753e8
dmathieu <dmathieu@salesforce.com>
2017-07-20 14:51:33 +0200

    simplify the rows/cols calculation

--- pdf.go
+++ pdf.go
@@ -2,33 +2,25 @@ package main
 
 import (
 	"errors"
+	"math"
 
 	"github.com/jung-kurt/gofpdf"
 )
 
 func generatePage(pdf *gofpdf.Fpdf, data []string) error {
 	pdf.AddPage()
-	n := nbColLines(data)
-	if n == 0 {
-		return errors.New("Invalid number of data. Cannot calculate equal number of columns and lines")
+	n := math.Sqrt(float64(len(data)))
+	if n != float64(int(n)) {
+		return errors.New("Invalid data number. Need a square number to get equal number of columns and lines")
 	}
 
 	for i, v := range data {
-		pdf.CellFormat(float64(275/n), float64(179/n), v, "1", 0, "C", false, 0, "")
+		pdf.CellFormat(275/n, 179/n, v, "1", 0, "C", false, 0, "")
 
-		if (i+1)%n == 0 {
+		if (i+1)%int(n) == 0 {
 			pdf.Ln(-1)
 		}
 	}
 
 	return nil
-}
-
-func nbColLines(data []string) int {
-	for i := 2; i <= 10; i++ {
-		if len(data)/i == i {
-			return i
-		}
-	}
-	return 0
 }
