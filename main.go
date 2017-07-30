73fa9480ba5a3ef03343982ab0ae43d9fb9e0df3
dmathieu <42@dmathieu.com>
2017-07-30 18:35:32 +0200

    change default grid and font size

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 func main() {
 	var size int
 	var in, out string
-	flag.IntVar(&size, "size", 6, "size of the table (number of rows and cols)")
+	flag.IntVar(&size, "size", 5, "size of the table (number of rows and cols)")
 	flag.StringVar(&in, "in", "", "input filename, in csv format")
 	flag.StringVar(&out, "out", "", "filename for the output, in pdf format")
 	flag.Parse()
 
 	pdf := gofpdf.New("L", "mm", "A4", "")
-	pdf.SetFont("Arial", "B", 16)
+	pdf.SetFont("Arial", "B", 12)
 
 	data, err := parseCsv(in)
 	if err != nil {
